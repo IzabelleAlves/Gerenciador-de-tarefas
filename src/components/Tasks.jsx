88b2571ed432e55070aa3bf59b62171a339fb4ee
Izabelle Alves <izabelle.alvesbl@gmail.com>
2025-03-31 08:26:15 -0300

    Atualiza o uso do destructuring

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,13 +1,13 @@
 import { ChevronRightIcon, TrashIcon } from "lucide-react";
 
-function Tasks(props) {
-  console.log(props);
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
+  //destructuring as props
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
-      {props.tasks.map((task) => (
+      {tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
           <button
-            onClick={() => props.onTaskClick(task.id)}
+            onClick={() => onTaskClick(task.id)}
             className={`bg-slate-400 w-full text-left text-white p-2 rounded-md 
               ${task.isCompleted && "line-through"}
               `}
@@ -18,7 +18,7 @@ function Tasks(props) {
             <ChevronRightIcon />
           </button>
           <button
-            onClick={() => props.onDeleteTaskClick(task.id)}
+            onClick={() => onDeleteTaskClick(task.id)}
             className="bg-slate-400 p-2 rounded-md text-white"
           >
             <TrashIcon />
