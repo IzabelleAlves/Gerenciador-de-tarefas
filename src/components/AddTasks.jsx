8251ad513c332777004e1ef5755bcaea045ed578
Izabelle Alves <izabelle.alvesbl@gmail.com>
2025-04-01 23:11:27 -0300

    Adicionada funcionalidade do botão 'adicionar' a tarefa

--- src/components/AddTasks.jsx
+++ src/components/AddTasks.jsx
@@ -1,4 +1,9 @@
-function AddTasks() {
+import { useState } from "react";
+
+function AddTasks({ onAddTaskSubmit }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
   return (
     <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
       <input
@@ -7,6 +12,8 @@ function AddTasks() {
         name=""
         id=""
         className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
       />
 
       <input
@@ -15,9 +22,14 @@ function AddTasks() {
         name=""
         id=""
         className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
       />
 
-      <button className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium">
+      <button
+        className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium"
+        onClick={() => onAddTaskSubmit(title, description)}
+      >
         Adicionar
       </button>
     </div>
