831db9efc4d3269831a2429301c57a8979bdb255
Izabelle Alves <izabelle.alvesbl@gmail.com>
2025-04-04 12:49:21 -0300

    Adiciona router

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import TaskPage from "./pages/taskPage.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />, //o element vai ser a pag principal
+  },
+  {
+    path: "/task",
+    element: <TaskPage />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
 
